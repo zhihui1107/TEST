c9eeebbef1eb7315a54fbdd398a9878a7529706a
zhihui1107 <759960833@qq.com>
2020-09-03 09:44:35 +0800

    springboot测试提交

---
+++ src/main/java/com/example/mes_skill_training/MesSkillTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mes_skill_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesSkillTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesSkillTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mes_skill_training/MesSkillTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mes_skill_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesSkillTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
